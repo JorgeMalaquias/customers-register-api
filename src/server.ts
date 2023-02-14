f54eb135d3ae2af27a6f4085c89d5b47d0608dbc
jorge <jorgemalaquia@gmail.com>
2023-02-14 16:19:45 -0300

    feat: routes index created

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import "express-async-errors";
 import cors from "cors";
+import routes from "./routes";
 
 const server = express();
 
 
 server.use(express.json());
 server.use(cors());
+
+server.use(routes);
 
 server.listen(process.env.PORT, () => {
     console.log(`server running on port ${process.env.PORT}`);
