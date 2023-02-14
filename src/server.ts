44f5fbeec454eddb0913edace87a04b1fb4acc14
jorge <jorgemalaquia@gmail.com>
2023-02-14 16:16:16 -0300

    chore: env dependecie added

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,13 @@
+import express from "express";
+import "express-async-errors";
+import cors from "cors";
+
+const server = express();
 
 
-console.log('working')
+server.use(express.json());
+server.use(cors());
+
+server.listen(process.env.PORT, () => {
+    console.log(`server running on port ${process.env.PORT}`);
+})
